b58242e7421296212c155e038eb0fd9245167425
cosmasobi <cosmasobi67@gmail.com>
2024-11-07 21:11:53 -0500

    im good to go!

--- src/main/java/blank.java
+++ src/main/java/blank.java
@@ -1,5 +1,8 @@
 public class blank {
     public static void main(String[] args) {
         System.out.println("RUN THIS TO TEST IF IT WORKS");
+        System.out.println("IF WORKS, TYPE YOUR NAME AS A STRING");
+        System.out.println("COSMAS");
+        System.out.println("EZRA");
     }
 }
