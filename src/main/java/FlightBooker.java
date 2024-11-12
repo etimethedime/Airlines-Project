6bc65614ca7f7f2698ccb9f1ac1bf269707feba2
mikiyasmekasha <mikiyasmekasha12@gmail.com>
2024-11-11 21:41:41 -0500

    name added MM

---
+++ src/main/java/FlightBooker.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class FlightBooker {
+    private ArrayList flightsAvailable;
+    private ArrayList flightsBooked;
+    private ArrayList seatsAvailable;
+    private ArrayList seatsBooked;
+    private ArrayList accommodations;
+
+    public FlightBooker(){
+
+    }
+
+}
